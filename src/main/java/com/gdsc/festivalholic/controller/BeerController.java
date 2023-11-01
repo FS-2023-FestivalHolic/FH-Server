e484a7de3ee27e446640ce791a07245970408627
changha <mentenseoul@gmail.com>
2023-11-01 16:00:42 +0900

    feat : yml파일 ignore

--- src/main/java/com/gdsc/festivalholic/controller/BeerController.java
+++ src/main/java/com/gdsc/festivalholic/controller/BeerController.java
@@ -4,6 +4,7 @@ package com.gdsc.festivalholic.controller;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.gdsc.festivalholic.config.response.ResponseDto;
 import com.gdsc.festivalholic.config.response.ResponseUtil;
+import com.gdsc.festivalholic.controller.dto.beer.BeerListResponseDto;
 import com.gdsc.festivalholic.controller.dto.beer.BeerResponseDto;
 import com.gdsc.festivalholic.controller.dto.beer.BeerSaveRequestDto;
 import com.gdsc.festivalholic.controller.dto.beerImage.BeerImageUploadDto;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -42,6 +45,15 @@ public class BeerController {
         BeerResponseDto beerById = beerService.findBeerById(beerId);
 
         return ResponseUtil.SUCCESS("맥주 정보 조회를 완료하였습니다.", beerById);
+
+    }
+
+    @GetMapping("")
+    public ResponseDto<List<BeerListResponseDto>> findAllBeer(){
+
+        List<BeerListResponseDto> allBeer = beerService.findAllBeer();
+
+        return ResponseUtil.SUCCESS("맥주 정보 조회를 완료하였습니다.", allBeer);
 
     }
 
