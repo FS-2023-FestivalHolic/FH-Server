f8ad1c2a6f753da2d179b89f880741e5b84d988d
changha <mentenseoul@gmail.com>
2023-11-02 19:40:28 +0900

    feat : findAllBeer의 beerId추가

--- src/main/java/com/gdsc/festivalholic/service/BeerService.java
+++ src/main/java/com/gdsc/festivalholic/service/BeerService.java
@@ -73,6 +73,7 @@ public class BeerService {
             Beer beer = all.get(i);
 
             BeerListResponseDto beerListResponseDto = BeerListResponseDto.builder()
+                    .beerId(beer.getId())
                     .beerName(beer.getBeerName())
                     .hashTagList(getHashTagNamesFromBeer(beer))
                     .likeNum(0)
