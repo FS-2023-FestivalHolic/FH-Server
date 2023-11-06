501f0d6f3b488a5098ab4119704c7ac98694192c
changha <mentenseoul@gmail.com>
2023-11-06 15:13:33 +0900

    feat : QRCode 생성 API추가

--- src/main/java/com/gdsc/festivalholic/controller/BeerController.java
+++ src/main/java/com/gdsc/festivalholic/controller/BeerController.java
@@ -10,10 +10,17 @@ import com.gdsc.festivalholic.controller.dto.beerImage.BeerImageUploadDto;
 import com.gdsc.festivalholic.domain.hashTag.HashTag;
 import com.gdsc.festivalholic.service.BeerService;
 import com.gdsc.festivalholic.service.HashTagService;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.Set;
 
@@ -75,6 +82,5 @@ public class BeerController {
         List<BeerListResponseDto> beers = beerService.findBeersByHashTags(hashTagIds);
         return ResponseUtil.SUCCESS("입력받은 해쉬태그 기준으로 맥주 정보 조회를 완료하였습니다.", beers);
     }
-
 
 }
